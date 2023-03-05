210673201b3c05cdc446c44927c424ef5dfe389c
Aditya <adityashah192105@gmail.com>
2023-03-06 00:41:28 +0530

    Bug fixes

--- blog/urls.py
+++ blog/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
 
     re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
-    path('qwertyrehadi/', admin.site.urls),
+    path('abcdadireh/', admin.site.urls),
     path('', include('blogapp.urls')),
 
 ]
